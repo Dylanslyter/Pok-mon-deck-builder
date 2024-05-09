8baacd8f13b2c34ca36bef2122ef24454d9b6574
Matt-Yee <matt.yee9@gmail.com>
2024-05-09 15:47:25 -0400

    defined express session, sequelize, and handlebars

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-Copy code
 const express = require('express');
 const app = express();
 const port = 3000;
+const expressHandlebars = require('express-handlebars');
+const routes = require('./controllers/api');
+const path = require('path');
+const session = require('express-session');
+// const helpers = require('./utils/helpers'); // not sure if needed
 
-app.use(express.static('public'));
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const handlebars = expressHandlebars.create({ helpers });
+
+// Define middleware here
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define session
+// const sess  = {
+//     secret: 'Super secret secret', //not sure how to hash this
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new sequelizeStore({
+//         db: sequelize
+//     })};
+
+// app.use(session(sess));
 
 // Define routes
-app.use('/api', require('./routes/authRoutes'));
+app.use('/api', require('./controllers/authRoutes'));
+
+// Define handlebars
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
