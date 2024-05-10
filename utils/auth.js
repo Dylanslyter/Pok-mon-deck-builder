9aad452f7b8aa4086694889ef064b5849a422a7a
Matt-Yee <matt.yee9@gmail.com>
2024-05-09 21:51:16 -0400

    added to the utils/auth.js file

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// copied code from mini project
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
