cf6010c09c2d7c5d5c3bce27800891a6935550f2
dylan <dylan.slyter@yahoo.com>
2024-05-14 18:14:53 -0400

    getting our paged to work now just need to creat buttons for them

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,5 +3,13 @@ const axios = require('axios');
 async function home(req, res) {
     res.render("homepage", {pageTitle: "pokémon deck builder"})
 }
-
-module.exports = { home };
+async function deck(req, res) {
+    res.render("deckpage", {pageTitle: "pokémon deck builder"})
+}
+async function favorite(req, res) {
+    res.render("favoritespage", {pageTitle: "pokémon deck builder"})
+}
+async function team(req, res) {
+    res.render("teamspage", {pageTitle: "pokémon deck builder"})
+}
+module.exports = { home, deck, team, favorite };
