255b00e3f48349003b53244854de9242226ee711
dylan <dylan.slyter@yahoo.com>
2024-05-13 20:36:50 -0400

    fixing heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { sequelize } = require('./config/connection'); 
 const path = require('path');
 const cookieParser = require('cookie-parser');
