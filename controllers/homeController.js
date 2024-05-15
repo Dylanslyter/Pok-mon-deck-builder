438e46fe0e9d9659fe22eb7ad244388c68a15e37
dylan <dylan.slyter@yahoo.com>
2024-05-14 20:06:25 -0400

    pushing

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,12 +4,28 @@ async function home(req, res) {
     res.render("homepage", {pageTitle: "pokémon deck builder"})
 }
 async function deck(req, res) {
-    res.render("deckpage", {pageTitle: "pokémon deck builder"})
+    res.render("deckspage", {pageTitle: "pokémon deck builder"})
 }
 async function favorite(req, res) {
     res.render("favoritespage", {pageTitle: "pokémon deck builder"})
 }
-async function team(req, res) {
-    res.render("teamspage", {pageTitle: "pokémon deck builder"})
+async function pokemonList(req, res) {
+    const base = "https://pokeapi.co/api/v2/";
+    const path = "pokemon?limit=100000&offset=0";
+
+    const response = await axios.get(`${base}${path}`);
+    const data = response.data;
+    console.log(data)
+    res.render("pokemonListpage", {pageTitle: "pokémon deck builder", data: data})
 }
-module.exports = { home, deck, team, favorite };
+async function pokemonDetail(req, res) {
+ 
+        const { name } = req.params; 
+        const path = `https://pokeapi.co/api/v2/pokemon/${name}`;
+        const response = await axios.get(path);
+        console.log(response.data)
+        res.render("pokemonDetailpage", { pageTitle: "Pokémon Deck Builder", data:response.data });
+
+}
+module.exports = { home, deck, pokemonList,
+    pokemonDetail, favorite };
